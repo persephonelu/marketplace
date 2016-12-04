5176aab02b04ee7039ad667fd56f717e17be9647
Qianwen <qianwen.xie.us@gmail.com>
2016-12-04 15:48:34 -0800

    update first page

--- scripts/services/services.js
+++ scripts/services/services.js
@@ -9,6 +9,9 @@
  7  */
 
 var gamearts_url='http://www.gamearts.me/';
+var codejob_url='http://www.codejob.tech/';
+var ourpets_url='http://ourpets.qianwenxie.com/';
+var iph_url='http://yaoxingliu.com/';
 
 angular.module('marketplace_services', [])
 
@@ -32,6 +35,50 @@ angular.module('marketplace_services', [])
             }
         };
     })
+    .factory('codejob', function ($resource) { //codejob factory
+        return {
+            get_codejob_products:function () {
+                return $resource(codejob_url + 'getCodeJobProducts.php');
+            },
+
+            get_codejob_products_by_ID:function(id){
+                return $resource(codejob_url + 'getCodeJobProducts.php?id=' + id.toString());//query
+            },
+            get_codejob_top5_products:function() {
+                return $resource(codejob_url + 'getCodejobTop5Products.php');
+            }
+        };
+    })
+
+    .factory('ourpets', function ($resource) { //ourpets
+        return {
+            get_ourpets_products:function () {
+                return $resource(ourpets_url + 'productRestful.php');
+            },
+
+            get_ourpets_products_by_ID:function(id){
+                return $resource(ourpets_url + 'productRestful.php?id=' + id.toString());//query
+            },
+
+            get_ourpets_products_by_Clickcout:function(id){
+                return $resource(ourpets_url + 'productRestful.php?id=' + id.toString());//query
+            }
+        };
+    })
+
+    .factory('iph', function ($resource) { //iph
+        return {
+            get_iph_products:function () {
+                return $resource(iph_url + 'productRestful.php');
+            },
+
+            get_iph_products_by_ID:function(id){
+                return $resource(iph_url + 'productRestful.php?id=' + id.toString());//query
+            }
+        };
+    })
+
+
     .factory('currProduct', function() {
         var currProduct = {};
         return currProduct;
